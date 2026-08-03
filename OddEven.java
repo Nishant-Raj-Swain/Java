9ba3275a5e69c46e04e18feacb5ccd464173655d
Nishant-Raj-Swain <nishantrajswain@gmail.com>
2026-08-03 17:29:53 +0000

    modified:   OddEven.java

--- OddEven.java
+++ OddEven.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 class OddEven{
 
     OddEven(int x){
@@ -12,8 +13,13 @@ class OddEven{
 
 class Main{
     public static void main(String args[]){
+        
+        Scanner sc = new Scanner(System.in);
 
-        OddEven obj = new OddEven(8);
+        System.out.println("Enter a no. : ");
+        int x = sc.nextInt();
+
+        OddEven obj = new OddEven(x);
 
     }
 }

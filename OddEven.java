b30c4d83cd57191decfe730c8664ffd55900fbe2
Nishant-Raj-Swain <nishantrajswain@gmail.com>
2026-08-04 15:59:07 +0000

    modified:   OddEven.java

--- OddEven.java
+++ OddEven.java
@@ -1,26 +1,24 @@
 import java.util.Scanner;
 class OddEven{
-
-    OddEven(int x){
-        if (x%2==0){
-            System.out.println("Even");
-        }
-
-        else {
-            System.out.println( "Odd");
-        }
-    }
-
-class Main{
     public static void main(String args[]){
-        
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("Enter a no. : ");
-        int x = sc.nextInt();
+        System.out.print("Enter a no. : ");
+        int n = sc.nextInt();
 
-        OddEven obj = new OddEven(x);
+        int Even_sum =0;
+        int Odd_sum = 0;
 
+        for(int i=0;i<n;i++){
+            if(i%2 == 0){
+                Even_sum +=i; 
+            }
+            else{
+                Odd_sum+=i;
+            }
+     
+        }
+        System.out.println("Even no. sum ="+Even_sum);
+        System.out.print("Odd no. sum  ="+Odd_sum);
     }
-}
 }
